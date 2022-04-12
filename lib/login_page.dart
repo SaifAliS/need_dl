97e00c94740fa3f3cb077d110b49059558c7ad65
Vamshidhar <dev.vamshidhar@gmail.com>
2022-04-12 12:09:10 +0530

    login done and other screens started

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:need_dl/supervisor_screen.dart';
+import 'package:need_dl/user_screen.dart';
+import 'package:need_dl/worker_screen.dart';
+
+enum SingingCharacter { User, Worker, Supervisor }
 
 class Login extends StatefulWidget {
   Login({Key? key}) : super(key: key);
@@ -10,6 +15,7 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
+  SingingCharacter? _character = SingingCharacter.User;
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -43,89 +49,150 @@ class _LoginState extends State<Login> {
             ],
           ),
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 23),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(25, 7, 0, 0),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Sign into your account",
-                style: TextStyle(fontSize: 13, color: Colors.grey),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(25, 7, 0, 0),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "YOUR EMAIL/PHONE NUMBER",
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              height: 50,
-              padding: EdgeInsets.fromLTRB(25, 7, 25, 0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Color.fromRGBO(224, 224, 224, 1),
-                  filled: true,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.fromLTRB(25, 7, 0, 0),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "PASSWORD",
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 50,
-              padding: EdgeInsets.fromLTRB(25, 7, 25, 0),
-              child: TextField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Color.fromRGBO(224, 224, 224, 1),
-                  filled: true,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              child: Container(
-                padding: EdgeInsets.fromLTRB(25, 7, 0, 0),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  "Forgot Password?",
+                  "Login",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Sign into your account",
+                  style: TextStyle(fontSize: 13, color: Colors.grey),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "SELECT YOUR ROLE",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ListTile(
+                      title: const Text('User'),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.User,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      title: const Text('Worker'),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.Worker,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Radio<SingingCharacter>(
+                    value: SingingCharacter.Supervisor,
+                    groupValue: _character,
+                    onChanged: (SingingCharacter? value) {
+                      setState(() {
+                        _character = value;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Supervisor',
+                      style: TextStyle(
+                        fontSize: 16,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "YOUR EMAIL/PHONE NUMBER",
                   style: TextStyle(fontSize: 14),
                 ),
               ),
-              onTap: () {},
-            ),
-          ],
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    fillColor: Color.fromRGBO(224, 224, 224, 1),
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "PASSWORD",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 50,
+                child: TextField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    fillColor: Color.fromRGBO(224, 224, 224, 1),
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
         ),
         bottomSheet: Column(
           mainAxisSize: MainAxisSize.min,
@@ -164,7 +231,26 @@ class _LoginState extends State<Login> {
                   ),
                 ),
               ),
-              onTap: () {},
+              onTap: () {
+                if (_character == SingingCharacter.User) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => user()),
+                  );
+                }
+                else if(_character == SingingCharacter.Worker){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => worker()),
+                  );
+                }
+                else{
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => supervisor()),
+                  );
+                }
+              },
             ),
           ],
         ));
--- lib/worker_screen.dart
+++ lib/worker_screen.dart
@@ -1,1 +1,72 @@
 
+import 'package:flutter/material.dart';
+
+class worker extends StatefulWidget {
+  const worker({ Key? key }) : super(key: key);
+
+  @override
+  State<worker> createState() => _workerState();
+}
+
+class _workerState extends State<worker> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+title:Container(
+  
+                alignment: Alignment.topRight,
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/NeedDL.png"),
+                  ),
+                ),
+              ),            centerTitle: true,
+            elevation: 0.3,
+            bottom: TabBar(
+                unselectedLabelColor: Colors.amber,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicator: BoxDecoration(
+                    gradient: LinearGradient(
+                      
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                        colors: [Colors.amberAccent, Colors.white12]),
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.amber),
+                tabs: [
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("WORK POSTS"),
+                    ),
+                  ),
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("ONGOING"),
+                    ),
+                  ),
+                  
+                ]),
+          ),
+          body: TabBarView(children: [
+            Icon(Icons.apps),
+            Icon(Icons.movie),
+            
+          ]),
+        )
+     ));
+
+
+    
+  }
+}
